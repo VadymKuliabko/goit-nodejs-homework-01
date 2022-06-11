b68083c10de80d5fc720e02309c90bc5cb9939fd
Vadym-Kuliabko <vadilatele@gmail.com>
2022-06-11 15:33:49 +0200

    fixed files

--- contacts.js
+++ contacts.js
@@ -4,6 +4,9 @@ const ObjectID = require("bson-objectid");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
@@ -27,12 +30,24 @@ const addContact = async (name, email, phone) => {
     phone,
   };
   contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  updateContacts(contacts);
   return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [removeId] = contacts.splice(index, 1);
+  updateContacts(contacts);
+  return removeId;
 };
 
 module.exports = {
   listContacts,
   getContactById,
   addContact,
+  removeContact,
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.clear();
+const { Command } = require("commander");
 const contacts = require("./contacts");
 
 const invokeAktion = async ({ action, id, name, email, phone }) => {
@@ -15,6 +15,10 @@ const invokeAktion = async ({ action, id, name, email, phone }) => {
       const addNewContact = await contacts.addContact(name, email, phone);
       console.log(addNewContact);
       break;
+    case "remove":
+      const removeContactById = await contacts.removeContact(id);
+      console.log(removeContactById);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
@@ -22,11 +26,16 @@ const invokeAktion = async ({ action, id, name, email, phone }) => {
   }
 };
 
-//invokeAktion({ action: "list" });
-//invokeAktion({ action: "get", id: "4" });
-invokeAktion({
-  action: "add",
-  name: "Mango",
-  email: "mango@gmail.com",
-  phone: "322-22-22",
-});
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAktion(argv);
